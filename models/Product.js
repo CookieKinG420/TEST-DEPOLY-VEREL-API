19ef451d24b61109bd67f07807e3abbb34c81363
CookieKinG420 <89388636+CookieKinG420@users.noreply.github.com>
2023-09-15 14:29:48 +0700

    add image to products api commit

--- models/Product.js
+++ models/Product.js
@@ -4,6 +4,7 @@ const ProductSchema = new mongoose.Schema({
 prod_name : String,
 prod_desc : String,
 prod_price : Number,
+prod_image : String,
 updated_at : {type : Date, default: Date.now}
 })
 module.exports = mongoose.model('Product', ProductSchema)
