d669accbea93446d1b432571fe2b9173dbad29b9
CookieKinG420 <89388636+CookieKinG420@users.noreply.github.com>
2023-09-18 10:28:58 +0700

    add type to products api commit

--- models/Product.js
+++ models/Product.js
@@ -5,6 +5,7 @@ prod_name : String,
 prod_desc : String,
 prod_price : Number,
 prod_image : String,
+prod_type : String,
 updated_at : {type : Date, default: Date.now}
 })
 module.exports = mongoose.model('Product', ProductSchema)
