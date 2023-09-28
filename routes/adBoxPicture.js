31af14e5bb34d2c4e7c02cc0e99f53a5b48a43a6
CookieKinG420 <89388636+CookieKinG420@users.noreply.github.com>
2023-09-28 11:20:50 +0700

    add adBox update api routes commit

--- routes/adBoxPicture.js
+++ routes/adBoxPicture.js
@@ -24,6 +24,19 @@ router.delete('/:id', async(req,res,next) =>{
     }
 })
 
+router.put('edit/:id', async(req, res, next)=> {
+    try{
+        const update = await adBox.findByIdAndUpdate(req.params.id,req.body, {new: true});
+        if(!update){
+            return res.status(404).json({message:'Ad not found!!'});
+        }
+        res.json(update);
+    }catch(err){
+        next(err);
+
+    }
+})
+
 router.post('/create', async (req, res, next)=>{
     try{
         const adPost = await adBox.create(req.body);
